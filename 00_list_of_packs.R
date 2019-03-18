7fca2f3588024bbb95b5c9bc083e4d5327488e0d
adfmb <actuario.alfredo.mb@gmail.com>
2019-03-18 12:50:43 -0600

    adding package dplyr

--- 00_list_of_packs.R
+++ 00_list_of_packs.R
@@ -1,1 +1,1 @@
-paqueterias<-list("shiny"="1.2.0","rmarkdown"="1.12","flexdashboard"="0.5.1.1","ggplot2"="3.1.0")
+paqueterias<-list("shiny"="1.2.0","rmarkdown"="1.12","flexdashboard"="0.5.1.1","ggplot2"="3.1.0","dplyr"="0.8.0.1")
