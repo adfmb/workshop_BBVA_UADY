7987a846fc8688a42f8bd2fd0ebd0841e0267f6f
adfmb <actuario.alfredo.mb@gmail.com>
2019-04-11 21:42:10 -0500

    Update files of session 01

--- 00_list_of_packs.R
+++ 00_list_of_packs.R
@@ -1,7 +1,13 @@
 paqueterias<-list("shiny"="1.2.0",
                   "rmarkdown"="1.12",
                   "flexdashboard"="0.5.1.1",
-                  "ggplot2"="3.1.0",
+                  "ggplot2"="3.1.1",
                   "dplyr"="0.8.0.1",
-                  "plotly"="4.8.0")
+                  "plotly"="4.8.0",
+                  "tidyverse"="1.2.1",
+                  "sparklyr"="1.0.0",
+                  "recipes"="0.1.5",
+                  "dbplyr" = "1.3.0",
+                  "janeaustenr"="0.1.5")
+spark_install("2.4.0")
 paqueterias
